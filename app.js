071b704ca6ff4264dbd5fa0b3ae48146c9889252
Wetpineapple <leonard.towne@outlook.com>
2022-01-17 16:48:09 -0600

    startProject

---
+++ app.js
@@ -0,0 +1,6 @@
+// Your JavaScript goes here!
+// console.log("Hello, World!")
+let message;
+message = "this is a test message"; //declare the message variable
+alert(message); //gives an alert of the variable let
+//output is the message variable "this is a test message"
